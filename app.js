c3f6725f4e0c1d1335b54c28286ec3b7de190441
Haider Ali <haiderali@192.168.10.19>
2022-05-26 08:50:30 -0700

    first version

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const morgan = require("morgan");
+const { nanoid } = require("nanoid");
+const mongoose = require("mongoose");
+const app = express();
+
+require("dotenv").config();
+
+const urls = mongoose.Schema({
+  urlSlug: String,
+  url: {
+    type: String,
+    required: true,
+  },
+  shorUrl: {
+    type: String,
+    required: true,
+  },
+});
+const UrlModel = mongoose.model("Urls", urls);
+
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(conn.connection.host);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectDb();
+app.set("view engine", "ejs");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/url", async (req, res) => {
+  const { url } = req.body;
+  let urlSlug = nanoid(5);
+  const slugExist = UrlModel.findOne({ urlSlug });
+  if (slugExist) {
+    urlSlug = nanoid(5);
+  }
+  const tinyUrl = await UrlModel.create({
+    urlSlug,
+    url,
+    shorUrl: req.protocol + "://" + req.get("host") + "/" + urlSlug,
+  });
+  if (tinyUrl) {
+    res.json(tinyUrl.shorUrl);
+  }
+});
+
+app.get("/:id", async (req, res) => {
+  const slug = req.params.id;
+  const urlExists = await UrlModel.findOne({ urlSlug: slug });
+  if (urlExists) {
+    res.redirect(urlExists.url);
+  } else {
+    res.send("not found");
+  }
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("listning...");
+});
